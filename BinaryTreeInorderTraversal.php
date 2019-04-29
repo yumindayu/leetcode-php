922500d013bdb1079485d928cca696ba25a3299b
lky <liyuanye@liyuanyedeMBP.lan>
2019-04-29 23:20:58 +0800

    add new file

---
+++ BinaryTreeInorderTraversal.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($value) { $this->val = $value; }
+ * }
+ */
+class Solution {
+
+  /**
+   * @param TreeNode $root
+   * @return Integer[]
+   * @github https://github.com/yumindayu/leetcode-php
+
+           1
+          / \
+          2  3
+         / \ /
+        4  5 6
+   */
+
+  function inorderTraversal($root) {
+    
+  }
+}
