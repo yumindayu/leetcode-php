cf4d83328e46b52906cc8ffadeaade7986cbb17d
lky <liyuanye@liyuanyedeMBP.lan>
2019-05-12 17:58:32 +0800

    add new file

---
+++ ValidateBinarySearchTree.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($value) { $this->val = $value; }
+ * }
+ */
+class Solution {
+
+  /**
+   * @param TreeNode $root
+   * @return Boolean
+   *
+   * @github https://github.com/yumindayu/leetcode-php
+
+           4
+          / \
+          2  6
+         / \ /
+        1  3 5
+   */
+  function isValidBST($root) {
+    
+  }
+
+
+}
