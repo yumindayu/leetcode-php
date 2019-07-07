dd43c32869d30c9c8062987600e7ecdb28720c5f
lky <liyuanye@liyuanyedeMBP.lan>
2019-07-07 14:22:45 +0800

    add new file

---
+++ SymmetricTree.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($value) { $this->val = $value; }
+ * }
+ */
+class Solution {
+
+  /**
+   * @param TreeNode $root
+   * @return Boolean
+   *
+   * @github https://github.com/yumindayu/leetcode-php
+          1
+         / \
+        2   2
+       / \ / \
+      3  4 4  3
+   */
+  function isSymmetric($root) {
+    
+  }
+}
