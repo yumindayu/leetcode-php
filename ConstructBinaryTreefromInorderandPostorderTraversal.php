116de609a13ce4a8d0399626286659fa401c2d42
lky <liyuanye@liyuanyedeMBP.lan>
2019-08-28 17:35:35 +0800

    add new file

---
+++ ConstructBinaryTreefromInorderandPostorderTraversal.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($value) { $this->val = $value; }
+ * }
+ */
+class Solution {
+
+  /**
+   * @param Integer[] $preorder
+   * @param Integer[] $inorder
+   * @return TreeNode
+
+    1
+   / \
+  2   3
+ /   / \
+ 4   5  6
+  \     /
+   7   8
+
+preOrder  [1,2,4,7,3,5,6,8]
+
+inOrder   [4,7,2,1,5,3,8,6]
+
+postOrder [7,4,2,5,8,6,3,1]
+
+
+    @github https://github.com/yumindayu/leetcode-php
+  */
+  function buildTree($inorder, $postorder) {
+    
+  }
+}
+
+
+
+
+
