c52182267d733c17b80449251c84fe4d16e1782d
yumin <liuwanglei2001@gmail.com>
2020-07-26 21:44:01 +0800

    update array size

--- 329.php
+++ 329.php
@@ -27,20 +27,21 @@ class Solution
 
     public function _dfs($matrix, $i, $j)
     {
-        if ($this->visited[$i * $this->cols + $j] != 0) {
-            return $this->visited[$i * $this->cols + $j];
+        $index = $i * $this->cols + $j;
+        if ($this->visited[$index] != 0) {
+            return $this->visited[$index];
         }
-        $this->visited[$i * $this->cols + $j]++;
+        $this->visited[$index]++;
         // $this->visited[$i * count($matrix[0]) + $j]++;
         $direction = [[-1, 0], [0, -1], [1, 0], [0, 1]];
         foreach ($direction as $d) {
             $x = $i + $d[0];
             $y = $j + $d[1];
             if ($x >= 0 && $x < count($matrix) && $y >= 0 && $y < count($matrix[0]) && $matrix[$x][$y] > $matrix[$i][$j]) {
-                $this->visited[$i * $this->cols + $j] = max($this->visited[$i * $this->cols + $j], $this->_dfs($matrix, $x, $y) + 1);
+                $this->visited[$index] = max($this->visited[$index], $this->_dfs($matrix, $x, $y) + 1);
             }
         }
-        return $this->visited[$i * $this->cols + $j];
+        return $this->visited[$index];
     }
 
 }
