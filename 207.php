885d04e8629a104d0115f8ea6ce53e95f41f7c91
yumin <liuwanglei2001@gmail.com>
2020-08-04 16:20:44 +0800

    remove noused code

--- 207.php
+++ 207.php
@@ -13,6 +13,7 @@ class Solution
      */
     public function canFinish($numCourses, $prerequisites)
     {
+        //将起点存在hash
         foreach ($prerequisites as $v) {
             if (isset($this->edges[$v[1]])) {
                 array_push($this->edges[$v[1]], $v[0]);
@@ -22,6 +23,7 @@ class Solution
         }
         for ($i = 0; $i < $numCourses; $i++) {
             if (!isset($this->visited[$i])) {
+                //不在visited里代表未搜索 每次从未搜索状态出发
                 $this->dfs($i);
             }
         }
@@ -34,15 +36,16 @@ class Solution
             return;
         }
 
-        $this->visited[$u] = 1;
+        $this->visited[$u] = 1; //将状态改为搜索中
         foreach ($this->edges[$u] as $v) {
             if (!isset($this->visited[$v])) {
                 $this->dfs($v);
             } elseif ($this->visited[$v] == 1) {
+                //如果相邻的节点状态是搜索中说明遇到环了
                 $this->invalid = true;
             }
         }
 
-        $this->visited[$u] = 2;
+        $this->visited[$u] = 2; //如果一切正常将状态改为搜索完毕
     }
 }
