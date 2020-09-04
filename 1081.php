69df0262d88050bde907eef7fdae2c9c6e32e1ba
yumin <liuwanglei2001@gmail.com>
2020-09-04 10:57:34 +0800

    fix 1081 solution

--- 1081.php
+++ 1081.php
@@ -3,61 +3,32 @@ class Solution
 {
 
     /**
-     * @param String $s
+     * @param String $text
      * @return String
      */
-    public function removeDuplicateLetters($s)
+    public function smallestSubsequence($text)
     {
         $count = [];
-        for ($i = 0; $i < strlen($s); $i++) {
-            if (isset($count[$s[$i]])) {
-                $count[$s[$i]]++;
+        for ($i = 0; $i < strlen($text); $i++) {
+            if (isset($count[$text[$i]])) {
+                $count[$text[$i]]++;
             } else {
-                $count[$s[$i]] = 1;
+                $count[$text[$i]] = 1;
             }
         }
         $visited = []; //这个visited可以不需要，假如不需要visited下面也可以通过判断是不是在stack中，此时需要用到in_array,不是O(1)的时间复杂度，leetcode的结果也能说明，两个方法都列出来，可以比较一下
         $stack   = [];
-        for ($i = 0; $i < strlen($s); $i++) {
-            $count[$s[$i]]--;
-            if (isset($visited[$s[$i]])) {
+        for ($i = 0; $i < strlen($text); $i++) {
+            $count[$text[$i]]--;
+            if (isset($visited[$text[$i]])) {
                 continue;
             }
-            while (!empty($stack) && end($stack) > $s[$i] && $count[end($stack)] > 0) {
+            while (!empty($stack) && end($stack) > $text[$i] && $count[end($stack)] > 0) {
                 unset($visited[end($stack)]);
                 array_pop($stack);
             }
-            $visited[$s[$i]] = true;
-            array_push($stack, $s[$i]);
-        }
-        return implode("", $stack);
-    }
-
-    /**
-     * @param String $s
-     * @return String
-     */
-    public function removeDuplicateLetters2($s)
-    {
-        $count = [];
-        for ($i = 0; $i < strlen($s); $i++) {
-            if (isset($count[$s[$i]])) {
-                $count[$s[$i]]++;
-            } else {
-                $count[$s[$i]] = 1;
-            }
-        }
-
-        $stack = [];
-        for ($i = 0; $i < strlen($s); $i++) {
-            $count[$s[$i]]--;
-            if (in_array($s[$i], $stack)) {
-                continue;
-            }
-            while (!empty($stack) && end($stack) > $s[$i] && $count[end($stack)] > 0) {
-                array_pop($stack);
-            }
-            array_push($stack, $s[$i]);
+            $visited[$text[$i]] = true;
+            array_push($stack, $text[$i]);
         }
         return implode("", $stack);
     }
