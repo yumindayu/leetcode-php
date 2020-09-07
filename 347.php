2276b6ebfd42b6342550c165f965031473af2c06
yumin <liuwanglei2001@gmail.com>
2020-09-07 13:08:51 +0800

    add 347 solution

---
+++ 347.php
@@ -0,0 +1,59 @@
+<?php
+class Solution
+{
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $k
+     * @return Integer[]
+     */
+    public function topKFrequent($nums, $k)
+    {
+        $record = [];
+        for ($i = 0; $i < count($nums); $i++) {
+            if (isset($record[$nums[$i]])) {
+                $record[$nums[$i]]++;
+            } else {
+                $record[$nums[$i]] = 1;
+            }
+        }
+        $pq = new PQ;
+        $i  = 0;
+        foreach ($record as $key => $value) {
+            if ($i >= $k) {
+                break;
+            }
+            $pq->insert($key, $value);
+            $i++;
+            unset($record[$key]);
+        }
+        $pq->setExtractFlags(PQ::EXTR_BOTH);
+        if (!empty($record)) {
+            foreach ($record as $key => $value) {
+                $top = $pq->top();
+                if ($value > $top['priority']) {
+                    $pq->extract();
+                    $pq->insert($key, $value);
+                }
+            }
+        }
+        $res = [];
+        while ($pq->valid()) {
+            array_unshift($res, $pq->current()['data']);
+            $pq->next();
+        }
+        return $res;
+    }
+}
+
+class PQ extends \SplPriorityQueue
+{
+    public function compare($priority1, $priority2)
+    {
+        if ($priority1 === $priority2) {
+            return 0;
+        }
+
+        return $priority1 < $priority2 ? 1 : -1;
+    }
+}
