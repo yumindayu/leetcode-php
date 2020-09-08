5a5712910a6fd02be46e8e2d16fffbe64d1d3b1f
yumin <liuwanglei2001@gmail.com>
2020-09-08 18:20:02 +0800

    add 1109 solution

---
+++ 1109.php
@@ -0,0 +1,24 @@
+<?php
+class Solution
+{
+
+    /**
+     * @param Integer[][] $bookings
+     * @param Integer $n
+     * @return Integer[]
+     */
+    public function corpFlightBookings($bookings, $n)
+    {
+        $nums = array_fill(0, $n, 0);
+        foreach ($bookings as $booking) {
+            $nums[$booking[0] - 1] += $booking[2];
+            if ($booking[1] < $n) {
+                $nums[$booking[1]] -= $booking[2];
+            }
+        }
+        for ($i = 1; $i < $n; $i++) {
+            $nums[$i] += $nums[$i - 1];
+        }
+        return $nums;
+    }
+}
