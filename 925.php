0aca7b8146985fd6cf471364480992052a3e8111
yumin <liuwanglei2001@gmail.com>
2020-10-21 08:55:55 +0800

    add 925 solution

---
+++ 925.php
@@ -0,0 +1,33 @@
+<?php
+class Solution
+{
+
+    /**
+     * @param String $name
+     * @param String $typed
+     * @return Boolean
+     */
+    public function isLongPressedName($name, $typed)
+    {
+        $i     = 0; //name字符串的指针
+        $index = 0; //typed字符串的指针
+        while ($index < strlen($typed)) {
+            if ($name[$i] == $typed[$index]) {
+                $i++;
+                $index++;
+            } else {
+                if ($index == 0) {
+                    return false;
+                }
+
+                if ($typed[$index] != $typed[$index - 1]) {
+                    return false;
+                }
+
+                $index++;
+            }
+        }
+        //最后如果name都验证完了没有遗漏的字符则是true
+        return $i == strlen($name);
+    }
+}
