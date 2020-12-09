7a229538818e70a86fb0c46dd467a18c57ed3bac
yumin <liuwanglei2001@gmail.com>
2020-12-09 10:48:57 +0800

    update unique path solution

--- UniquePathsII.php
+++ UniquePathsII.php
@@ -1,33 +1,35 @@
 <?php
-class Solution {
+class Solution
+{
 
-  /**
-   * @param Integer[][] $obstacleGrid
-   * @return Integer
-   */
-  function uniquePathsWithObstacles($obstacleGrid) {
-    $m = count($obstacleGrid);
-    $n = count($obstacleGrid[0]);
-    $dp = [];
-    if ($obstacleGrid[$m - 1][$n - 1] == 1) return 0;
-    $dp[$m -1][$n - 1] = 1;
-    for ($i = $m - 1; $i >= 0; $i--) {
-      for ($j = $n - 1; $j >= 0;  $j--) {
-        if ($obstacleGrid[$i][$j] == 1) {
-          $dp[$i][$j] = 0;
-        } else {
-          if ($i != $m - 1 || $j != $n - 1) {
-            if (isset($dp[$i][$j + 1]) && isset($dp[$i + 1][$j])) {
-              $dp[$i][$j] = $dp[$i][$j + 1] + $dp[$i + 1][$j];
-            } else if (isset($dp[$i][$j + 1])) {
-              $dp[$i][$j] = $dp[$i][$j + 1];
-            } else {
-              $dp[$i][$j] = $dp[$i + 1][$j];
+    /**
+     * @param Integer[][] $obstacleGrid
+     * @return Integer
+     */
+    public function uniquePathsWithObstacles($obstacleGrid)
+    {
+        $m = count($obstacleGrid);
+        $n = count($obstacleGrid[0]);
+
+        $dp = array_fill(0, $m, array_fill(0, $n, 0));
+        for ($i = 0; $i < $m; $i++) {
+            for ($j = 0; $j < $n; $j++) {
+                if ($obstacleGrid[$i][$j] == 1) {
+                    continue;
+                } elseif ($i == 0 && $j == 0) {
+                    //入口
+                    $dp[$i][$j] = 1;
+                } elseif ($i == 0) {
+                    //第一行
+                    $dp[$i][$j] = $dp[$i][$j - 1];
+                } elseif ($j == 0) {
+                    //第一列
+                    $dp[$i][$j] = $dp[$i - 1][$j];
+                } else {
+                    $dp[$i][$j] = $dp[$i - 1][$j] + $dp[$i][$j - 1];
+                }
             }
-          }
         }
-      }
+        return $dp[$m - 1][$n - 1];
     }
-    return $dp[0][0];
-  }
 }
