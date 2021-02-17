5e43e7efc3c4cd3451399e1961b37332e8e99767
yumin <liuwanglei2001@gmail.com>
2021-02-17 10:10:55 +0800

    add 561 solution

---
+++ 566.php
@@ -0,0 +1,49 @@
+<?php
+class Solution
+{
+
+    /**
+     * @param Integer[][] $nums
+     * @param Integer $r
+     * @param Integer $c
+     * @return Integer[][]
+     */
+    public function matrixReshape($nums, $r, $c)
+    {
+        $m = count($nums);
+        $n = count($nums[0]);
+        if ($r * $c != $m * $n) {
+            return $nums;
+        }
+
+        $ans = [];
+        for ($i = 0; $i < $m * $n; $i++) {
+            $ans[$i / $c][$i % $c] = $nums[$i / $n][$i % $n];
+        }
+        return $ans;
+
+        // $m = count($nums);
+        // $n = count($nums[0]);
+        // if ($r * $c != $m * $n) {
+        //     return $nums;
+        // }
+
+        // $rows = [];
+        // for ($i = 0; $i < $m; $i++) {
+        //     for ($j = 0; $j < $n; $j++) {
+        //         $rows[] = $nums[$i][$j];
+        //     }
+        // }
+        // $ans = [];
+        // $k   = 0;
+        // for ($i = 0; $i < $r; $i++) {
+        //     $array = [];
+        //     for ($j = 0; $j < $c; $j++) {
+        //         $array[] = $rows[$k];
+        //         $k++;
+        //     }
+        //     $ans[] = $array;
+        // }
+        // return $ans;
+    }
+}
